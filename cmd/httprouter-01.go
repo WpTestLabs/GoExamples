f0e5e5e134891e4dc98b10f427f95bcae98dcb60
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-11-13 17:28:48 -0500

    Create httprouter-01.go

---
+++ cmd/httprouter-01.go
@@ -0,0 +1,30 @@
+package main
+// Exaample from: https://stevenwhite.com/building-a-rest-service-with-golang-2/
+
+import (
+	// Standard library packages
+	"net/http"
+
+	// Third party packages
+	"github.com/julienschmidt/httprouter"
+	"github.com/swhite24/go-rest-tutorial/controllers"
+)
+
+func main() {
+	// Instantiate a new router
+	r := httprouter.New()
+
+	// Get a UserController instance
+	uc := controllers.NewUserController()
+
+	// Get a user resource
+	r.GET("/user/:id", uc.GetUser)
+
+	r.POST("/user", uc.CreateUser)
+
+	r.DELETE("/user/:id", uc.RemoveUser)
+
+	// Fire up the server
+	http.ListenAndServe("localhost:3000", r)
+}
+
